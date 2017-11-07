063b99836298fff4ba3c3e9fc52c572e2a7e03c2
kreee <huminghua4@gmail.com>
2017-11-07 20:46:27 +0800

    first

---
+++ src/main/java/com/humh/foods/specialsnacks/SpecialSnacksApplication.java
@@ -0,0 +1,12 @@
+package com.humh.foods.specialsnacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialSnacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialSnacksApplication.class, args);
+	}
+}
